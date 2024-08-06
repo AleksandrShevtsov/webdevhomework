2a20ba24fcedfd0e26a8ea6f546cda60c6cc4713
Chief <aleksandersfevtsov85@gmail.com>
2024-08-06 11:07:46 +0200

    Update admin models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-import myapp.models
-
-# Register your models here.
-admin.site.register(myapp.models.Task)
-admin.site.register(myapp.models.Category)
-admin.site.register(myapp.models.SubTask)
+from .models import Task, SubTask, Category
 
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('status', 'deadline', 'created_at')
+    ordering = ('-created_at',)
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status', 'deadline', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('status', 'deadline', 'created_at')
+    ordering = ('-created_at',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
