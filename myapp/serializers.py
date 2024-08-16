14254a67428a08a020aaaf24eca4b0aa7ff72b39
Chief <aleksandersfevtsov85@gmail.com>
2024-08-16 21:43:02 +0200

    task 1

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -12,3 +12,11 @@ class TaskCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         exclude = ['created_at']
+
+
+class SubTaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = ['id', 'title', 'description', 'status', 'deadline']
+        read_only_fields = ['created_at']
+
