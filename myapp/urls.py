3f1fe806b343393e4be0b3f501b1c7d6ce0c7539
Chief <aleksandersfevtsov85@gmail.com>
2024-08-17 07:09:35 +0200

    home work 12, task 5

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('tasks/create', create_task, name='create_tasks'),
-    path('tasks/', list_tasks, name='list_tasks'),
-    path('tasks/stats', task_stats, name='task_stats'),
-    path('tasks/<int:pk>', task_details, name='task_details'),
 
-]
+    path('tasks/', TaskListView.as_view()),
+    path('tasks/<int:pk>/', TaskDetailView.as_view()),
+    path('subtasks/', SubTaskListCreateView.as_view()),
+    path('subtasks/<int:pk>/', SubTaskDetailUpdateCreateView.as_view()),
+ ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,56 +1,83 @@
-from django.core.paginator import Paginator
-from django.db.models import *
-from django.utils import timezone
-from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import *
-
+from rest_framework import status
 from .models import Task, SubTask, Category
 from .serializers import *
 
 
-@api_view(['POST'])
-def create_task(request):
-    serializer = TaskCreateSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=HTTP_201_CREATED)
-    return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+class TaskListView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# Создайте эндпойнт для получения списка задач с фильтрацией по статусу и дедлайну. Реализуйте пагинацию результатов.
-#
-# Шаги для выполнения:
-#
-# Создайте представление для получения списка задач с фильтрами и пагинацией.
-#
-# Создайте маршрут для обращения к представлению.
-@api_view(['GET'])
-def list_tasks(request):
-    tasks = Task.objects.filter(status__in=['New', 'In progress'], deadline__gt=timezone.now())
-    paginator = PageNumberPagination()
-    paginator.page_size = 2  # Количество элементов на странице
-    result_page = paginator.paginate_queryset(tasks, request)
-    serializer = TaskSerializer(result_page, many=True)
-    return paginator.get_paginated_response(serializer.data)
+class TaskDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskDetailSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskDetailSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
 
 
-@api_view(['GET'])
-def task_details(request, pk):
-    task = Task.objects.get(pk=pk)
-    serializer = TaskSerializer(task)
-    return Response(serializer.data, status=HTTP_200_OK)
+class SubTaskListCreateView(APIView):
+    def get(self, request):
+        subtasks = SubTask.objects.all()
+        serializer = SubTaskSerializer(subtasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SubTaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-def task_stats(request):
-    total_tasks = Task.objects.count()
-    status_counts = Task.objects.values('status').annotate(count=Count('status'))
-    overdue_tasks = Task.objects.filter(deadline__lt=timezone.now(), status__in=['New', 'In progress']).count()
+class SubTaskDetailUpdateCreateView(APIView):
+    def get_object(self, pk):
+        try:
+            return SubTask.objects.get(pk=pk)
+        except SubTask.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    return Response({
-        'total_tasks': total_tasks,
-        'status_counts': status_counts,
-        'overdue_tasks': overdue_tasks
-    })
+    def get(self, request, pk):
+        subtask = self.get_object(pk)
+        serializer = SubTaskSerializer(subtask)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        subtask = self.get_object(pk)
+        serializer = SubTaskSerializer(subtask, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        subtask = self.get_object(pk)
+        subtask.delete()
